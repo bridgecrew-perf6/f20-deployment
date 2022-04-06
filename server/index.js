8d9a3d2c6dba8517a293e133b95bda33723504d7
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-06 10:05:35 -0700

    wuit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('Public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
